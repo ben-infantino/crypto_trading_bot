184e1282473f56c6fa74efbec301a0bde77f263d
binfantino97 <bennyinfantino99@gmail.com>
2025-01-23 15:02:01 -0500

    Commit data staging

---
+++ src/data_staging/config.py
@@ -0,0 +1,4 @@
+from src.shared.config import source_of_truth_data_folder, data_folder 
+
+# base assets to organize folders into subfolders for ingestion / processing
+base_assets = ["XRP", "ETH", "SOL"]
---
+++ src/shared/config.py
@@ -0,0 +1,9 @@
+import os
+
+base_drive = "E:/"
+
+base_folder = os.path.join(base_drive, "TRADER_1_DATA")
+
+source_of_truth_data_folder = os.path.join(base_folder, "source_of_truth_data")
+
+data_folder = os.path.join(base_folder, "data")
